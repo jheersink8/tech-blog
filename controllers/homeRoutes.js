8755abaca208bdb53c54928cb5e7a4aa20fbd4cf
jheersink8 <150620321+jheersink8@users.noreply.github.com>
2024-03-30 20:11:57 -0600

    Comments and Blog posts routing correctly.

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,14 +1,61 @@
 
 const router = require('express').Router();
+const { Blog, User, Comment } = require('../models');
 
+// Route for homepage nav link which renders all blogs in a list
+router.get('/', async (req, res) => {
+    try {
+        const blogData = await Blog.findAll({
+            include: [
+                {
+                    model: User,
+                    attributes: ['username']
+                }
+            ]
+        });
 
+        const blogs = blogData.map((blog) => blog.get({ plain: true }));
 
+        res.render('homepage', {
+            blogs
+        });
 
-
-// Route for homepage nav link
-router.get('/', (req, res) => {
-    res.render('homepage');
+    } catch (err) {
+        res.status(500).json(err);
+    };
 });
+
+// Route for a single blog post with comments section
+router.get('/blog/:id', async (req, res) => {
+    try {
+        const blogData = await Blog.findByPk(req.params.id, {
+            include: [
+                {
+                    model: User,
+                    attributes: ['username'],
+                },
+                {
+                    model: Comment,
+                    attributes: ['content', 'date'],
+                    include: [
+                        {
+                            model: User,
+                            attributes: ['username']
+                        }
+                    ]
+                }],
+        });
+        const blog = blogData.get({ plain: true });
+ 
+        res.render('blog', {
+            ...blog
+            // ADD LOGGED IN LOGIC LATER
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
+
 
 // Route for dashboard nav link
 router.get('/dashboard', (req, res) => {
---
+++ utils/helpers.js
@@ -0,0 +1,6 @@
+module.exports = {
+    // Helper to format dates as MM/DD/YYYY
+    format_date: (date) => {
+        return date.toLocaleDateString();
+    }
+};
